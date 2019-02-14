0947c4c6dc28692c784b4c79b37f7a779724a6fc
hejtmy <hejtmy@gmail.com>
2019-02-14 01:51:43 +0100

    Adds data file and setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='navr',
+      version='0.1',
+      description='Navigation related analytical and graphing functions',
+      url='http://github.com/hejtmy/navr-python',
+      author='Lukáš "hejtmy" Hejtmánek',
+      author_email='hejtmy@gmail.com',
+      license='MIT',
+      packages=['navr'],
+      install_requires=[
+            'pandas',
+            'numpy',
+            'matplotlib',
+      ],
+      zip_safe=False)
