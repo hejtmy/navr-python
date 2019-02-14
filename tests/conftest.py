f2f026e584cb8f9ec5a39cffe3091802299b08a1
hejtmy <hejtmy@gmail.com>
2019-02-14 04:36:13 +0100

    Fixes preprocessing and testsL

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 import pandas as pd
+from navr import navr
 
 
 @pytest.fixture(scope="session")
@@ -7,3 +8,9 @@ def example_data():
     path = 'data/bva_walking.csv'
     pd_data = pd.read_csv(path)
     return pd_data
+
+
+@pytest.fixture(scope="session")
+def navr_class():
+    pd_data = example_data()
+    return navr.Navr(pd_data)
--- tests/test_loading.py
+++ tests/test_loading.py
@@ -1,6 +1,5 @@
 from navr import navr
-import pandas as pd
-import pytest
+
 
 def test_class_initialisation(example_data):
     n = navr.Navr(example_data)
